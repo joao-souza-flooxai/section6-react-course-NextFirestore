af59087f84b4c383c0f4477f4a7b0566c6dbefc0
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-16 13:48:50 -0300

    feat: Using module css in components.

---
+++ next-firestore/src/pages/stylist.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Stylist.module.css";
+
+export default function Stylist(){
+    return(
+        <div className={styles.roxo}>
+            <h1>CSS module</h1>
+        </div>
+    )
+}
