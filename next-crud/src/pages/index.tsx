6ce42837e16a539e685e3eab5a67319aab9a72d6
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-22 12:46:02 -0300

    chore: initial project setup after a long debugginon enviroment. feat: added the home page example.

---
+++ next-crud/src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white"
+    >
+      <h1>App</h1>
+    </div>
+  );
+}
